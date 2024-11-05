17776019fcf513c0d4b9264c51bc6070d185dbdf
PhanVanDiej <22520182@gm.uit.edu.vn>
2024-11-05 14:31:01 +0700

    add demoPrototype

---
+++ DemoPrototype/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPrototype
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Type { get; set; }
+
+        public abstract Employee GetClone();
+        public abstract void ShowDetails();
+    }
+}
